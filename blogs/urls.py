85039625ff7e968f65d3234406bcf90027fffbb3
PydevG <peterndindi41@gmail.com>
2025-02-06 16:44:16 +0300

    third commit

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
-from django.views import View
 
 urlpatterns = [
     path('', views.homeview, name='Home'),
+    path('about/', views.aboutview, name='about'),
+    path('contact/', views.contactview, name='contact'),
+    path('register/', views.registrationview, name='register'),
+    path('login/', views.loginview, name='login'),
+    path('post/', views.singlepostview, name='single-post'),
+    path('category/', views.categoryview, name='category'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -4,3 +4,22 @@ from django.views import View
 
 def homeview(request):
     return render(request, 'blogs/index.html')
+
+def singlepostview(request):
+    return render(request, 'blogs/single-post.html')
+
+def registrationview(request):
+    return render(request, 'blogs/registration.html')
+
+def loginview(request):
+    return render(request, 'blogs/login.html')
+
+
+def categoryview(request):
+    return render(request, 'blogs/category.html')
+
+def aboutview(request):
+    return render(request, 'blogs/about.html')
+
+def contactview(request):
+    return render(request, 'blogs/contact.html')
